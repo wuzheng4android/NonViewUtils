9330d7523f5e446e394b0c7704d0a581d92eb591
吴征同学 <wuzheng0420@126.com>
2015-05-15 11:04:34 +0800

    测试提交

--- NonViewUtils/app/src/main/java/org/android/share/utils/example/MainActivity.java
+++ NonViewUtils/app/src/main/java/org/android/share/utils/example/MainActivity.java
@@ -25,6 +25,8 @@ public class MainActivity extends QuickBaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //测试
+
+
         setContentView(R.layout.activity_main);
     }
 
